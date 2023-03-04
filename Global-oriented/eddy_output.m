1a70c02f406a08877cf8f32b85801da3fce49636
Yk-Yang <88022893+Yk-Yang@users.noreply.github.com>
2023-03-04 17:45:13 +0800

    Add files via upload

--- Global-oriented/eddy_output.m
+++ Global-oriented/eddy_output.m
@@ -8,10 +8,18 @@ for i=1:length(eddy)
     time=eddy(i).time;
     if length(time)>=1
         eval([strcat(polarity,'.time'),'=eddy(i).time;'])
-        eval([strcat(polarity,'.bound'),'=eddy(i).bound;'])
-        eval([strcat(polarity,'.center'),'=eddy(i).center;'])
+        if strfind(hemi,'S')==1
+            cen=eddy(i).center; bou=eddy(i).bound;
+            [center,bound]=correct_bound_south(cen,bou);
+            eval([strcat(polarity,'.bound'),'=bound;'])
+            eval([strcat(polarity,'.center'),'=center;'])
+        else
+            eval([strcat(polarity,'.bound'),'=eddy(i).bound;'])
+            eval([strcat(polarity,'.center'),'=eddy(i).center;'])
+        end
         eval([strcat(polarity,'.radius'),'=eddy(i).radius;'])
         eval([strcat(polarity,'.McS'),'=eddy(i).McS;'])
+        
         switch polarity
             case  'AE'
                 save([path,'/Output/',hemi,'/',year,'/',polarity,'_',num2str(length(eddy_out)+k)],'AE')
