23cd2e962b888eadba676f80924b5b28f4d53c65
Yk-Yang <88022893+Yk-Yang@users.noreply.github.com>
2025-07-16 20:03:11 +0800

    Update Main_program.m

--- Global-oriented/Main_program.m
+++ Global-oriented/Main_program.m
@@ -1,5 +1,32 @@
 clear; close all; clc
 
+%% 
+% The automatic scale?selective eddy identification algorithm (SEIA).
+%--------------------------------------------------------------------------  
+%  Author: Yikai Yang (yangyikai@scsio.ac.cn) 
+%  [Ver 1  Jan 2022]
+%  Please cite correctly:
+%  Yang, Y., Zeng, L. & Wang, Q. Assessment of global eddies from satellite
+%  data by a scale-selective eddy identification algorithm (SEIA). Clim Dyn
+%  62, 881–894 (2024).
+%--------------------------------------------------------------------------
+% Main_program.m
+%  |
+%  |--> topo_mask.m
+%  |
+%  |--> SEIA.m
+%  |     |
+%  |     |--> gridnanpeak.m
+%  |     |--> unique_points.m
+%  |     |--> radius_compute.m
+%  |     |--> intersection_ratio.m
+%  |     |--> mean_bound.m
+%  |     |--> eddy_output.m
+%  |     |--> cal_amp_eke_vor & onedimgrid (extra version)
+%--------------------------------------------------------------------------
+%  Note: Please ensure that the SLA data you prepare is consistent with the sample.
+%  Note: You need the global topo data named ETOPO1.mat which will be used by
+%  topo_mask.m containning 'lon (m)', 'lat (n)' and 'topo (mxn)'.
 %% Preset path and parameters
 % the path with profiles named 'SLA', 'Output' and 'SEIA'
 main_path='F:\SCSEddy\ÎÐÐýÕï¶Ï\Closed_Streamline\SEIA\SEIA_global'  
@@ -20,10 +47,12 @@ mask_n=topo_mask(main_path,area_n,rslt,mask_depth);
 mask_s=topo_mask(main_path,area_s,rslt,mask_depth);
 
 c=1;     % error-compensating correction
-L=125;  % half of the mesoscale, unit: km
+L=150;  % half of the mesoscale, unit: km
 r=6371;         % earth radius (km)
-d=2*pi*r*cosd(1:70)/360;      % distance per degree by latitude (km)
-min_points=10;                   % lower grid points of eddy boundary
+d_lon=2*pi*r/360;                 % distance per degree by longitude (km)
+d_lat=2*pi*r*cosd([1:70]')/360;      % distance per degree by latitude(1-70°) (km)
+d=(zeros(length(d_lat),1)+d_lon+d_lat)/2;         % approximately distance per degree (km)
+min_points=10;                    % lower grid points of eddy boundary
 max_points_lat=floor(2*pi*L./(rslt*d))+c; % upper grid points by latitude
 
 % among the tracking procedure
